3c37be63283c5efd5fc83cab42360f941c0094b0
Szabolcs Szilagyi <szabolcs.szilagyi@planorama.com>
2020-02-23 16:19:13 +0100

    setup to run both batched and non-batched stream

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const hl = require('highland');
 const { MongoClient } = require('mongodb');
 
 const url = 'mongodb://localhost:27017/highland_node_12_test';
+const shouldBatch = !!process.env.SHOULD_BATCH;
 
 const promiseTimeout = (toRace, delay) => {
   const timer = new Promise((resolve, reject) => {
-    console.log('timeout set')
     setTimeout(() => reject(new Error('time is up!')), delay);
   });
 
@@ -17,38 +17,44 @@ const promiseTimeout = (toRace, delay) => {
 async function run() {
   const client = new MongoClient(url, { useUnifiedTopology: true });
 
+  async function exit(error) {
+    if (error) console.log(error.message);
+    else console.log('Successfully finished');
+
+    await client.close();
+    process.exit(error ? 128 : 0);
+  }
+
   await client.connect();
   console.info('Connected successfully to server');
   const collection = client.db().collection('test');
 
   await collection.deleteMany({});
 
-  for (let counter = 0; counter < 8000; counter++) {
+  for (let counter = 0; counter < 1500; counter++) {
     await collection.insertOne({ foo: 'bar', counter });
   }
 
-  await collection.count({}).then(r => console.info('Inserted documents:', r));
+  await collection.count({})
+    .then(r => console.info('Inserted documents:', r, 'Batched:', shouldBatch));
 
   const mongoStream = collection
     .find()
-    .batchSize(100)
     .stream();
 
-  const stream = hl(mongoStream)
-    .batch(300)
-    .map(r => (console.count('seen'), Promise.resolve(r)))
+  const getStream = () => shouldBatch ?
+        hl(mongoStream).batch(300) :
+        hl(mongoStream);
+
+  const stream = getStream()
+    .map(r => (console.count('Processed document'), Promise.resolve(r)))
     .flatMap(hl)
     .collect()
     .toPromise(Promise);
 
-  await promiseTimeout(stream, 10000)
-    .catch(async (e) => {
-      console.error(e.message);
-      await client.close();
-      process.exit(128);
-    });
-
-  await client.close();
+  return promiseTimeout(stream, 5000)
+    .catch(exit)
+    .then(r => exit());
 }
 
 run();
